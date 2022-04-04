7dadb5e24b389ca1791617601abbd936a58c943a
oceanw1 <79440854+oceanw1@users.noreply.github.com>
2022-04-04 23:58:19 +0700

    Add files via upload

---
+++ src/app/login-signup-page/login-signup-page.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LoginSignupPageComponent } from './login-signup-page.component';
+
+describe('LoginSignupPageComponent', () => {
+  let component: LoginSignupPageComponent;
+  let fixture: ComponentFixture<LoginSignupPageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LoginSignupPageComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoginSignupPageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
